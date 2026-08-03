1d5b801ca286cbc85e55b0ca4acbdc35e645a866
Merwan Ibrahim <merwanibrahim@Merwans-MacBook-Air.local>
2026-08-02 19:03:04 -0700

    Complete React movie app with favorites and persistence

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/Favorites.css";
+import { useMovieContext } from "../movieContext";
+import Moviecard from "./Moviecard";
 
 const Favorites = () => {
-  return (
-    <div className="favorites-empty">
-      <h1>Favorites</h1>
-      <p>Click the heart icon to add movies to your favorites!</p>
-    </div>
-  );
+  const { favorites } = useMovieContext();
+
+  if (favorites.length !== 0) {
+    return (
+      <>
+        <h1 className="favorites-text">Favorite movies</h1>
+        <div className="movies-grid">
+          {favorites.map((movie) => {
+            return <Moviecard key={movie.id} movie={movie} />;
+          })}
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="favorites-empty">
+          <h1>Favorites</h1>
+          <p>Click the heart icon to add movies to your favorites!</p>
+        </div>
+      </>
+    );
+  }
 };
 
 export default Favorites;
